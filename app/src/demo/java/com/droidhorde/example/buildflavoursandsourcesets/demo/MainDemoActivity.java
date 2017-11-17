b6f533722b1d8755d1f36567ebb15da8f8248ffd
droidhorde <tjaart@droidhorde.com>
2017-11-17 20:38:34 +0200

    -- Added demo and full source set directories. -- Added demo and full launcher activity.

---
+++ app/src/demo/java/com/droidhorde/example/buildflavoursandsourcesets/demo/MainDemoActivity.java
@@ -0,0 +1,15 @@
+package com.droidhorde.example.buildflavoursandsourcesets.demo;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+import com.droidhorde.example.buildflavoursandsourcesets.R;
+
+public class MainDemoActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main_demo);
+    }
+}
---
+++ app/src/full/java/com/droidhorde/example/buildflavoursandsourcesets/full/MainFullActivity.java
@@ -0,0 +1,15 @@
+package com.droidhorde.example.buildflavoursandsourcesets.full;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+import com.droidhorde.example.buildflavoursandsourcesets.R;
+
+public class MainFullActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main_full);
+    }
+}
